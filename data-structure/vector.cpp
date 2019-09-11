35e887d41279d2e20c2da33bf994b1e6fd3298f3
liyingfei <1780810607@qq.com>
2019-09-11 18:28:10 +0800

    9.11

--- data-structure/vector.cpp
+++ data-structure/vector.cpp
@@ -20,5 +20,28 @@ int main()
 	for(int i = 0; i < obj.size(); i++)
 		cout<<obj[i] <<" ";
 	cout<<endl;
+	
+	cout<<"*****************************"<<endl;
+	vector<vector<int> > A;
+ 
+	vector<int> B;
+	B.push_back(0);
+	B.push_back(1);
+	B.push_back(2);
+ 
+	A.push_back(B);
+ 
+	B.clear();
+	B.push_back(3);
+	B.push_back(4);
+ 
+	A.push_back(B);
+	//vector<vector<int> >A中的vector元素的个数
+	int len = A.size();
+	cout<<len<<endl;
+
+	//vector<vector<int> >A中第i个vector元素的长度
+	len = A[0].size();
+	cout<<len<<endl;
 	return 0;
 }
